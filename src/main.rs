aa8a4c15006dd99adf33f82897d6398641ec4b2a
Jordan Doyle <jordan@doyle.la>
2020-05-15 02:49:48 +0100

    first param is actually a filename

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::Read;
+use std::path::Path;
 use syntect::highlighting::ThemeSet;
 use syntect::html::highlighted_html_for_string;
 use syntect::parsing::SyntaxSet;
@@ -19,13 +20,32 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn highlight(content: &str, ext: &str) {
+fn highlight(content: &str, filename: &str) {
     let ss = SyntaxSet::load_defaults_newlines();
     let ts = ThemeSet::load_defaults();
 
-    let syntax = ss
-        .find_syntax_by_extension(ext)
-        .unwrap_or_else(|| ss.find_syntax_plain_text());
+    let syntax = {
+        let path = Path::new(filename);
+        let file_name = path.file_name().and_then(|n| n.to_str()).unwrap_or("");
+        let extension = path.extension().and_then(|x| x.to_str()).unwrap_or("");
+        let ext_syntax = ss
+            .find_syntax_by_extension(file_name)
+            .or_else(|| ss.find_syntax_by_extension(extension));
+
+        let line_syntax = if ext_syntax.is_none() {
+            content
+                .lines()
+                .next()
+                .map(|v| ss.find_syntax_by_first_line(v))
+                .flatten()
+        } else {
+            None
+        };
+
+        ext_syntax.or(line_syntax)
+    };
+
+    let syntax = syntax.unwrap_or_else(|| ss.find_syntax_plain_text());
 
     println!(
         "{}",
