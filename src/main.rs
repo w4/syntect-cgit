7ae60be6e5c0f82eb2c83b8b37ca57c2b1627909
Jordan Doyle <jordan@doyle.la>
2020-05-15 02:16:52 +0100

    Read from stdin, first param is file ext

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
-use syntect::parsing::SyntaxSet;
+use std::io::Read;
 use syntect::highlighting::ThemeSet;
-use syntect::html::highlighted_html_for_file;
+use syntect::html::highlighted_html_for_string;
+use syntect::parsing::SyntaxSet;
 
-fn main() {
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        return Ok(());
+    }
+
+    let mut buffer = String::new();
+    let stdin = std::io::stdin();
+    let mut handle = stdin.lock();
+    handle.read_to_string(&mut buffer)?;
+
+    highlight(&buffer, &args[1]);
+
+    Ok(())
+}
+
+fn highlight(content: &str, ext: &str) {
     let ss = SyntaxSet::load_defaults_newlines();
     let ts = ThemeSet::load_defaults();
 
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {
-        return;
-    }
+    let syntax = ss
+        .find_syntax_by_extension(ext)
+        .unwrap_or_else(|| ss.find_syntax_plain_text());
 
-    let theme = &ts.themes["InspiredGitHub"];
-    let html = highlighted_html_for_file(&args[1], &ss, theme).unwrap();
-    println!("{}", html);
+    println!(
+        "{}",
+        highlighted_html_for_string(content, &ss, syntax, &ts.themes["InspiredGitHub"])
+    );
 }
